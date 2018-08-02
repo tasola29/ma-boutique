b8be247606b58176569e1eb2fc8558c3bce8f423
Solenn LARZUL <solennlarzul@gmail.com>
2018-08-02 13:57:26 +0200

    Modification titre en H1

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
     </nav>
   
 	<section class="container ma-section">
-		<h1 class="text-center mt-4 display-4">Bienvenue dans ma boutique</h1>
+		<h1 class="text-center mt-4 display-4">Welcome To All of You</h1>
 	</section>
 	
 		<form>
